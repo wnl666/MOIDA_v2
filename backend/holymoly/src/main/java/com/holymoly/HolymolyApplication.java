6989ab0fa78e5e40484f3fcc7170cc359777ebfc
wnl666 <wnl668@naver.com>
2022-04-12 23:54:10 +0900

    fix: 프로젝트 기본 모듈 생성

---
+++ backend/holymoly/src/main/java/com/holymoly/HolymolyApplication.java
@@ -0,0 +1,13 @@
+package com.holymoly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolymolyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolymolyApplication.class, args);
+    }
+
+}
---
+++ backend/holymoly/src/test/java/com/holymoly/HolymolyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holymoly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolymolyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
